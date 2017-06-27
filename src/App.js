d3046b59dbf7d2890635db0f16b84c849b37e092
Matt <matt.smith@digital.gov.au>
2017-06-27 13:34:39 +1000

    pancake sass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <header className="uikit-header" role="banner">
+          <h1 className="uikit-header-heading">Page title</h1>
+          <span className="uikit-header-subline">Service sub-title that could be a little longer</span>
+        </header>
+        <p>
+          <strong>uikit font:</strong>
+          <span class="font">The quick brown fox jumps over the lazy dog</span>
         </p>
+        <button className="uikit-btn">Continue</button>
       </div>
     );
   }
